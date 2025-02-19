69cb2e13a1e5c03e110b6838d2902fa210e50549
Mi-Py-1 <mikepymer@hotmail.co.uk>
2025-02-19 21:49:34 +0000

    resolved: view leading to feels loop. Corrected pathway in urls.py

---
+++ social_media_app/social/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('signup/', views.signup, name='signup'),
+    path('create_post/', views.create_post, name='create_post'),
+    path('update_post/<int:post_id>/', views.update_post, name='update_post'),
+    path('delete_post/<int:post_id>/', views.delete_post, name='delete_post'),
+    path('add_feel/<int:post_id>/', views.add_feel, name='add_feel'),
+]
