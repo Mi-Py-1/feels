bf714dc51dd342a32dbd16b03c562e9378204424
Mi-Py-1 <mikepymer@hotmail.co.uk>
2025-02-19 22:24:19 +0000

    add environment variables for secret key and database configuration

---
+++ social_media_app/feels/env.py
@@ -0,0 +1,4 @@
+import os
+
+os.environ['SECRET_KEY'] = 'wFA3d31Y6JhBxqy61FgTSPYT_PHhNvKB-etqAiIYpKzJ6oPNXeNHv-Yr0pzwDD9-Lfo'
+os.environ['DATABASE_URL'] = 'postgresql://neondb_owner:pBcbD30FTjat@ep-quiet-tree-a2kq9xqb.eu-central-1.aws.neon.tech/ebay_patch_vowel_812299'
--- social_media_app/feels/settings.py
+++ social_media_app/feels/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+import env  # Import env to load environment variables
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6971%8y@4*vj8i%+x858&(azqddaxkp_j+)#9s!xo^*e5i8^h2'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -78,10 +81,7 @@ WSGI_APPLICATION = 'feels.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
 
 
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
