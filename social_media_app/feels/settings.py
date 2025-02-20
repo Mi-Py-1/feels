9e8d67eb376ba52f860713e60bbe5a2b478be698
Mi-Py-1 <mikepymer@hotmail.co.uk>
2025-02-20 15:08:26 +0000

    connected log out button to homescreen and hid posts when logged out

--- social_media_app/feels/settings.py
+++ social_media_app/feels/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -123,3 +123,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGOUT_REDIRECT_URL = '/'
