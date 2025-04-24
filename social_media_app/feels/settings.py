0144d331558aedd18474942aa5f1a5977dbb7378
Mi-Py-1 <mikepymer@hotmail.co.uk>
2025-04-24 16:15:54 +0100

    changed debug to true to test heroku issue

--- social_media_app/feels/settings.py
+++ social_media_app/feels/settings.py
@@ -29,7 +29,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+DEBUG = True
+
+# os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ['feels.herokuapp.com', '127.0.0.1']
 
